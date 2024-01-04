b0f4c73d3fe672773c4865321aa981f2363be7f1
Edison Sanchez <edisondeveloper@hotmail.com>
2024-01-04 02:45:39 -0500

    Se hace estandar url base
    Se hace estandar url base

--- app/src/main/java/com/edisonsanchez/apppruebatecnicanexos/data/Constantes.kt
+++ app/src/main/java/com/edisonsanchez/apppruebatecnicanexos/data/Constantes.kt
@@ -1,6 +1,6 @@
 package com.edisonsanchez.apppruebatecnicanexos.data
 
-const val URL_BASE = "http://192.168.56.1:8080/api/payments/"
+const val URL_BASE = "http://localhost:8080/api/payments/"
 const val RESPONSE_OK = "00"
 const val NAME_TABLE_TRANSACTIONS = "Transactions"
 const val AUTHORIZATION_END_POINT = "authorization"
